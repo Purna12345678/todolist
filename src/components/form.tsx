815fe47df3f24b530edd1a4d187fd8f95fbdbdd1
Purna12345678 <PUYadav@Opteamix.com>
2025-03-21 15:41:29 +0530

    added the components in form forgot to uplode before

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,5 +1,9 @@
+
 import React, { useState } from "react";
 import "./form.css";
+import Input from "../components/reusablecomponents/Input";
+import Button from "../components/reusablecomponents/Button";
+import Card from "../components/reusablecomponents/Card";
 
 interface TodoItem {
   id: number;
@@ -9,6 +13,11 @@ interface TodoItem {
 
 const Form: React.FC = () => {
   const [list, setList] = useState<TodoItem[]>([]);
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value); 
+  };
 
   const AddToList = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter" && event.currentTarget.value !== "") {
@@ -16,7 +25,7 @@ const Form: React.FC = () => {
         ...list,
         { id: Date.now(), value: event.currentTarget.value, selected: false },
       ]);
-      event.currentTarget.value = "";
+      setInputValue("");
     }
   };
 
@@ -43,51 +52,9 @@ const Form: React.FC = () => {
     setList((prevList) => prevList.filter((item) => item.id !== id));
   };
 
-  const List = list.map((item) => (
-    <div className="listitem1" key={item.id}>
-      <input
-        type="checkbox"
-        className="item-checkbox"
-        checked={item.selected}
-        onChange={() => toggleSelection(item.id)}
-      />
-      {item.selected ? (
-        <s>
-          <li className="listitem2">{item.value}</li>
-        </s>
-      ) : (
-        <li className="listitem">{item.value}</li>
-      )}
-      {item.selected && (
-        <button className="remove-this" onClick={() => removeItem(item.id)}>
-          x
-        </button>
-      )}
-    </div>
-  ));
-
-  const Bottom: React.FC = () => {
-    if (list.length > 0) {
-      const selectedItems = list.filter((item) => item.selected).length;
-      return (
-        <div id="footer1">
-          <p id="count">{list.length} items left</p>
-          <button id="all">All</button>
-          <button id="active">Active</button>
-          <button id="completed">Completed</button>
-          {selectedItems > 0 && (
-            <button id="remove-selected" onClick={removeSelectedItems}>
-              Clear completed
-            </button>
-          )}
-        </div>
-      );
-    }
-    return null;
-  };
-
   return (
     <div className="box1">
+     
       <div>
         <input
           className="select-all"
@@ -95,13 +62,34 @@ const Form: React.FC = () => {
           onChange={toggleSelectAll}
           checked={list.length > 0 && list.every((item) => item.selected)}
         />
-        <input id="input" onKeyDown={AddToList} />
+        <Input
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={AddToList}
+        />
       </div>
-      <div>{List}</div>
-      <Bottom />
+
+     
+      <div>
+        {list.map((item) => (
+          <Card key={item.id} item={item} toggleSelection={toggleSelection} removeItem={removeItem} />
+        ))}
+      </div>
+
+      
+      {list.length > 0 && (
+        <div id="footer1">
+          <p id="count">{list.length} items left</p>
+          <Button text="All" />
+          <Button text="Active" />
+          <Button text="Completed" />
+          {list.some((item) => item.selected) && (
+            <Button text="Clear completed" variant="danger" onClick={removeSelectedItems} />
+          )}
+        </div>
+      )}
     </div>
   );
 };
 
 export default Form;
-
