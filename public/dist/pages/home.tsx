74d8d100c8dfd5fb2776f95507972f0a1a70ec81
Purna12345678 <PUYadav@Opteamix.com>
2025-04-02 00:29:38 +0530

    chore: changed the config to vite from webpack

--- public/dist/pages/home.tsx
+++ public/dist/pages/home.tsx
@@ -1,7 +1,7 @@
-import Logo from "../../src/components/logo";
+import Logo from "../../../src/components/logo";
 import React from "react";
-import Form from "../../src/components/form";
-import Footer from "../../src/components/footer";
+import Form from "../../../src/components/form";
+import Footer from "../../../src/components/footer";
 import './home.css'
 let Home = () => {
     return(
